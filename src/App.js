ffa67a4962574e0d00e500a6ca89f2004ecb91f2
Koray Dundar <koraydundar_40@hotmail.com>
2019-05-23 15:23:19 +0300

    Created Pokedex Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Pokedex from "./Pokedex";
 
-import Pokecard from './Pokecard';
-
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
-
-    
     return (
       <div className="App">
-        <Pokecard id={4} name="Charmander" type="fire" exp={62}/>
-
+        <Pokedex />
       </div>
     );
   }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
-import './Pokecard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+import React, { Component } from "react";
+import "./Pokecard.css";
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+// https://assets.pokemon.com/assets/cms2/img/pokedex/detail/
 
 class Pokecard extends Component {
-    render() {
-
-        let imgSrc = `${POKE_API}${this.props.id}.png`
-        return (
-            <div className="Pokecard">
-                <h1>{this.props.name}</h1>
-                <img src={imgSrc} alt={this.props.name} />
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.exp}</div>
-            </div>
-        )
-    }
+  render() {
+    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
+        <img src={imgSrc} alt={this.props.name} />
+        <div>Type: {this.props.type}</div>
+        <div>EXP: {this.props.exp}</div>
+      </div>
+    );
+  }
 }
 
-export default Pokecard
-
-
+export default Pokecard;
