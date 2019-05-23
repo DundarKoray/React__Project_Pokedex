8c0f2d9e7b89d923a156d9933dd31e285cabdc51
Koray Dundar <koraydundar_40@hotmail.com>
2019-05-23 16:21:57 +0300

    Replaced the old images with fancier images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
-import "./Pokecard.css";
-const POKE_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-// https://assets.pokemon.com/assets/cms2/img/pokedex/detail/
+import React, { Component } from "react"
+import "./Pokecard.css"
+
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+//   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+let padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number)
 
 class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
@@ -14,8 +16,8 @@ class Pokecard extends Component {
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">EXP: {this.props.exp}</div>
       </div>
-    );
+    )
   }
 }
 
-export default Pokecard;
+export default Pokecard
